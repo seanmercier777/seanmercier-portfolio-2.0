86e0039d192473bd52d1b12497362f24518f5090
mercis48 <seanmercier777@gmail.com>
2024-10-20 01:09:24 -0500

    Cool

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/three')
+def three():
+    return render_template('three.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
