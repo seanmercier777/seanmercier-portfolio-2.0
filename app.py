5497f96d048ba2b5b23ff284d82b1fc0f32e2bb3
mercis48 <seanmercier777@gmail.com>
2024-10-19 23:50:38 -0500

    looks ok

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import sass
 
 app = Flask(__name__)
+
+# sass.compile(dirname=('static/scss', 'static/css'), output_style='compressed')
 
 
 @app.route('/')
